837f587589f5e320df34454f4ebedef257475152
therasuldev <rasul.ramixanov@gmail.com>
2022-04-27 23:34:34 +0400

    spinkit color changed

--- lib/view/home/splash_page.dart
+++ lib/view/home/splash_page.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:ms/constant/constant.dart';
 import 'package:ms/view/widgets/widget.dart';
 
 class SplashScreen extends MSStatelessWidget {
   SplashScreen({Key? key}) : super(key: key);
-   static Route route() {
+  static Route route() {
     return MaterialPageRoute<void>(builder: (_) => SplashScreen());
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Image(image: AssetImage('assets/img/splash.png'))),
+    return Scaffold(
+      body: Center(child: SpinKitCircle(color: blackAccent)),
     );
   }
 }
